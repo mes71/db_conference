dfcd8dbb842cc9f2be9355f8682e80314117a4b1
mojtaba <mr.mojtaba47@gmail.com>
2024-10-12 09:07:52 +0330

    feat : database connection

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/db_conf";
+        String user = "root";
+        String password = "1234567";
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+
+            if (connection != null) {
+                Ansi.getInstance().printGreen("Connection established successfully");
+            }
 
 
+        } catch (SQLException e) {
+            Ansi.getInstance().printYellow("====> Connection failed");
+            Ansi.getInstance().printYellow("====> Error: " + e.getMessage());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
